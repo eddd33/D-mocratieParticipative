bb878db124648111838dbc09bf81da5a710cceeb
poiron2u <lea.poiron@telecomnancy.eu>
2021-12-02 15:51:13 +0100

    Amélioration du layout

--- site/appliweb.py
+++ site/appliweb.py
@@ -13,4 +13,9 @@ def citoyen():
 @app.route('/elu')
 def elu():
     return render_template('elu.html')
-
+@app.route('/accueil_c')
+def accueil_c():
+    return render_template('accueil_c.html')
+@app.route('/accueil_e')
+def accueil_e():
+    return render_template('accueil_e.html')
