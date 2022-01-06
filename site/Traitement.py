ff7ce1d1b217139aa59fc6baecf30beefe39c772
poiron2u <lea.poiron@telecomnancy.eu>
2022-01-06 17:00:22 +0100

    Commentaire des algorithmes de traitement

--- site/Traitement.py
+++ site/Traitement.py
@@ -1,10 +1,11 @@
 from ON import *
 
-def traitement_sociopro(P):
-        
-    yes=10*[0]
+#les algorithhmes ci-dessous permettent de transformer les résultats de requêtes SQL en listes utilisable pour l'affichage des données par nos algorithmes
+
+def traitement_sociopro(P):     # P est une liste de tuples dont le premier élément est la catégorie socio-professionnelle et dont le second élément est le vote
+    yes=10*[0]                  #On crée 2 listes de longueur le nombre de catégories, dont le n-ième élément sera le nombre de votes (respectivement "Oui" et "Non") pour la n-ième catégorie
     no=10*[0]
-    for k in range (len(P)):
+    for k in range (len(P)):    #On remplit les listes en lisant le vote et la catégorie récoltés pour chaque votant
         if P[k][1]=='Oui':
             if P[k][0]=='Etudiant':
                 yes[0]+=1
@@ -49,9 +50,9 @@ def traitement_sociopro(P):
                 no[9]+=1
     return yes,no
 
-def traitement_catembert(H):
-    p=10*[0]
-    for k in range (len(H)):
+def traitement_catembert(H):  #H est la liste des catégories socio-professionnelles des votants
+    p=10*[0]                  #On crée une liste de longueur le nombre de catégories, dont le n-ième élément sera le nombre de votes pour la n-ième catégorie
+    for k in range (len(H)):  #On remplit la liste en lisant la catégorie récoltée pour chaque votant
         if H[k][0]=='Etudiant':
             p[0]+=1
         if H[k][0]=='Retraité':
@@ -74,14 +75,13 @@ def traitement_catembert(H):
             p[9]+=1
     return p
 
-def traitement_parents(Y):
-    oui=2*[0]
+def traitement_parents(Y):    #Y est une liste de tuples dont le premier élément est la réponse à la question "Êtes vous parent?" et dont le second élément est le vote
+    oui=2*[0]                 #On crée 2 listes de longueur le nombre de catégories, dont le n-ième élément sera le nombre de votes (respectivement "Oui" et "Non") pour la n-ième situation
     non=2*[0]
-    for k in range (len(Y)):
-        
-        if Y[k][1]=='Oui':
-            if Y[k][0]=='oui':
-                oui[0]+=1
+    for k in range (len(Y)):  #On remplit les listes en lisant le vote et la situation familiale récoltés pour chaque votant
+        if Y[k][1]=='Oui':    
+            if Y[k][0]=='oui':   #Si la réponse à "Êtes vous parent?" est "oui" 
+                oui[0]+=1        #On ajoute un vote dans la liste oui en position 0
             if Y[k][0]=='non':
                 oui[1]+=1
         if Y[k][1]=='Non':
@@ -91,19 +91,19 @@ def traitement_parents(Y):
                 non[1]+=1
     return oui,non
 
-def traitement_parembert(Z):
-    p=2*[0]
-    for k in range (len(Z)):
+def traitement_parembert(Z):  #Z est la liste des situations familiales des votants
+    p=2*[0]                   #On crée une liste de longueur le nombre de situations, dont le n-ième élément sera la nombre de votes pour la n-ième situation
+    for k in range (len(Z)):  #On remplit la liste en lisant la situation récoltée pour chaque votant
         if Z[k][0]=='oui':
             p[0]+=1
         if Z[k][0]=='non':
             p[1]+=1
     return p
 
-def traitement_sexe(U):
-    oui=3*[0]
+def traitement_sexe(U):        #Y est une liste de tuples dont le premier élément est le sexe et dont le second élément est le vote
+    oui=3*[0]                  #On crée 2 listes de longueur le nombre de sexes, dont le n-ième élément sera le nombre de votes (respectivement "Oui" et "Non") pour le n-ième sexe
     non=3*[0]
-    for k in range (len(U)):
+    for k in range (len(U)):    #On remplit les listes en lisant le vote et le sexe récoltés pour chaque votant
         if U[k][1]=='Oui':
             if U[k][0]=='M.':
                 oui[0]+=1
@@ -120,9 +120,9 @@ def traitement_sexe(U):
                 non[2]+=1
     return oui,non
 
-def traitement_camembert_s(G):
-    p=3*[0]
-    for k in range (len(G)):
+def traitement_camembert_s(G):   #G est la liste des sexes des votants
+    p=3*[0]                      #On crée une liste de longueur le nombre de sexes, dont le n-ième élément sera la nombre de votes pour le n-ième sexe
+    for k in range (len(G)):     #On remplit la liste en lisant le sexe récolté pour chaque votant
         if G[k][0]=='M.':
             p[0]+=1
         if G[k][0]=='Mme.':
